bdfd2dfa3e2a495a63c3fa0aade91286c695e4a4
Ethereal <0_ethereal_0@protonmail.com>
2020-05-30 14:36:37 -0700

    Concatenate multi api service txs

--- scripts/get-transactions.js
+++ scripts/get-transactions.js
@@ -174,48 +174,30 @@ async function main() {
   	}
   }
 
-  function getTransactionsComposer(
+  async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+
+  function getTransactionHashesComposer(
     _txsUriBuilder,
     _internalTxsUriBuilder,
     _getHashFromApiServiceTx,
     _apiServiceName //for logging only
   ){
     return async (_address, _fromBlock, _toBlock) => {
-      let txsApiService = await makeSynchronousRequest(
+      let txsFromApiService = await makeSynchronousRequest(
         _txsUriBuilder(_address, _fromBlock, _toBlock)
       )
-      console.log('total tx for '+_address+' fetched from '+_apiServiceName+' api ',txsApiService.length)
-      let txs = [];
-      let internalTxs = [];
-      for (var i = 0; i < txsApiService.length; i++) {
-        let hash = _getHashFromApiServiceTx(txsApiService[i])
-        //console.log('Getting '+_apiServiceName+' tx info', hash, 'for address', _address)
-        await sleep(10);
-        let txToPush = await web3.eth.getTransaction(hash);
-        txToPush.receipt = await web3.eth.getTransactionReceipt(hash);
-        if (txToPush.receipt.logs)
-          txToPush.receipt.logs = logDecoder.decodeLogs(txToPush.receipt.logs)
-        txs.push(txToPush);
-      }
-      if(_apiServiceName=='etherscan'){
-        //TODO: Disable internal tx fetching until etherscan difference resolved
-        let internalTxsApiService = await makeSynchronousRequest(
-          _internalTxsUriBuilder(_address, _fromBlock, _toBlock)
-        )
-        console.log('Total internal tx for '+_address+' fetched from '+_apiServiceName+': ',internalTxsApiService.length)
-        for (var i = 0; i < internalTxsApiService.length; i++) {
-          let hash = _getHashFromApiServiceTx(internalTxsApiService[i])
-          //console.log('Getting '+_apiServiceName+' internal tx info', hash, 'for address', _address)
-          await sleep(10);
-          let internalTxToPush = await web3.eth.getTransactionReceipt(hash);
-          if (internalTxToPush.logs)
-            internalTxToPush.logs = logDecoder.decodeLogs(internalTxToPush.logs)
-          internalTxs.push(internalTxToPush);
-        }
-      }
+      let internalTxsFromApiService = await makeSynchronousRequest(
+        _internalTxsUriBuilder(_address, _fromBlock, _toBlock)
+      )
+      let txHashes = txsFromApiService.map(_getHashFromApiServiceTx)
+      let internalTxHashes = internalTxsFromApiService.map(_getHashFromApiServiceTx)
 
-      console.log('API Service:',_apiServiceName,'Total transactions:',txs.length,'; Total internal transactions:', internalTxs.length);
-      return { txs, internalTxs };
+      console.log('Address:',_address,'; Service:',_apiServiceName,'; Tx Hashes:',txHashes.length,'; Int Tx Hashes:', internalTxHashes.length);
+      return { txHashes, internalTxHashes };
     }
   }
 
@@ -245,7 +227,7 @@ async function main() {
     if(typeof(tx.hash)!=='undefined') return tx.hash
     if(typeof(tx.transactionHash)!=='undefined') return tx.transactionHash
     console.log('Failed tx: ',tx)
-    throw new Exception('Neither tx.hash nor tx.transactionHash found in blockscout tx.')
+    throw 'Neither tx.hash nor tx.transactionHash found in blockscout tx.'
   }
 
   function blockscoutTxsUriBuilder(_address,_fromBlock,_toBlock){
@@ -264,44 +246,61 @@ async function main() {
             +'&sort=asc'
   }
 
-  function getTransactionsMultiApiWithCheckComposer (
-    _txApiServices
+  function getTransactionsMultiApiComposer (
+    _txHashApiServices
   ) {
     return async (_address,_fromBlock,_toBlock) => {
-      let transactionsFetchedFromEachApiService = await Promise.all(
-        _txApiServices.map((func)=>func(_address,_fromBlock,_toBlock))
+      let txHashesFromEachApi =
+        await Promise.all(
+          _txHashApiServices.map((func)=>func(_address,_fromBlock,_toBlock))
+        )
+      let txHashes = _.union(
+        ...txHashesFromEachApi.map((apiHashes)=>apiHashes.txHashes)
       )
-      console.log("Cross checking tx...")
-      let txsFromFirstApiService = transactionsFetchedFromEachApiService[0].txs;
-      //TODO: Add checking for internal tx once difference between etherscan and blockscout resolved
-      transactionsFetchedFromEachApiService.forEach((transactionsFetched)=>{
-        if(transactionsFetched.txs.length !== txsFromFirstApiService.length )
-          throw new Exception("Number of transactions fetched differed between Api services.",transactionsFetched.txs.length,'vs',txsFromFirstApiService.length)
-        transactionsFetched.txs.forEach((tx,index)=>{
-          if(tx.hash !== txsFromFirstApiService[index].hash)
-            throw new Exception("Tx hash between api service providers differed.",tx.hash,'vs',txsFromFirstApiService[index].hash)
+      let internalTxHashes = _.union(
+        ...txHashesFromEachApi.map((apiHashes)=>apiHashes.internalTxHashes)
+      )
+      let txs = await Promise.all(
+        txHashes.map(async (hash,index)=>{
+          await sleep(100*index);
+          console.log('Getting tx ', hash, 'for address', _address)
+          let txToPush = await web3.eth.getTransaction(hash);
+          txToPush.receipt = await web3.eth.getTransactionReceipt(hash);
+          if (txToPush.receipt.logs)
+            txToPush.receipt.logs = logDecoder.decodeLogs(txToPush.receipt.logs)
+          return txToPush
         })
-      })
-      console.log("Cross check passed.\n")
-      return transactionsFetchedFromEachApiService[0]
+      )
+      let internalTxs = await Promise.all(
+        internalTxHashes.map(async (hash,index)=>{
+          await sleep(100*index);
+          console.log('Getting internal tx ', hash, 'for address', _address)
+          let internalTxToPush = await web3.eth.getTransactionReceipt(hash);
+          if (internalTxToPush.logs)
+            internalTxToPush.logs = logDecoder.decodeLogs(internalTxToPush.logs)
+          return internalTxToPush
+        })
+      )
+
+      return {txs, internalTxs}
     }
   }
 
-  let getBlockscoutTransactions = getTransactionsComposer(
+  let getBlockscoutTransactionHashes = getTransactionHashesComposer(
     blockscoutTxsUriBuilder,
     blockscoutInternalTxsUriBuilder,
     blockscoutGetHashFromApiServiceTx,
     "blockscout"
   )
 
-  let getEtherscanTransactions = getTransactionsComposer(
+  let getEtherscanTransactionHashes = getTransactionHashesComposer(
     etherscanTxsUriBuilder,
     etherscanInternalTxsUriBuilder,
     etherscanGetHashFromApiServiceTx,
     "etherscan"
   )
 
-  let getTransactions = getTransactionsMultiApiWithCheckComposer([getEtherscanTransactions,getBlockscoutTransactions])
+  let getTransactions = getTransactionsMultiApiComposer([getEtherscanTransactionHashes,getBlockscoutTransactionHashes])
 
   let transactionsFetched;
   console.log('Getting txs from controller..');
